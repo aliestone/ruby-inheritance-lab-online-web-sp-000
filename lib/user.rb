121fadb683f73665d738f134199129b0be325f88
Alie Stone <helloaliestone@gmail.com>
2020-10-08 15:22:03 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,9 @@
 class User
 
-  attr_accessor :name
+  attr_accessor :first_name, :last_name
 
-  initialize(name)
-    @name = first_name
+  initialize
+    @first_name = first_name
     @last_name = last_name
   end
+end 
